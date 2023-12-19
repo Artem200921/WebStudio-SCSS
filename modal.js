ea3944e2660da8d59ff7321155be8d2255ce8ebc
Artem Fedorov <artemfedorov1411@gmail.com>
2023-12-19 13:34:27 +0200

    media requests

--- modal.js
+++ modal.js
@@ -13,3 +13,27 @@
     document.body.classList.toggle("no-scroll");
   }
 })();
+
+
+// script.js
+
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("openModalBtn");
+// var span = document.getElementsByClassName("close")[0];
+// var backdrop = document.getElementById("backdrop");
+
+// btn.onclick = function() {
+//     modal.style.display = "block";
+//     backdrop.style.display = "block";
+// }
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+//     backdrop.style.display = "none";
+// }
+
+// backdrop.onclick = function() {
+//     modal.style.display = "none";
+//     backdrop.style.display = "none";
+// }
+
